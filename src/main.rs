81ff04950309df85c63809e422e2efd8df155203
Jack Pantalena <12242683+jpantalena@users.noreply.github.com>
2022-08-16 18:52:21 -0600

    Add Account struct; iterate over transactions; apply deposit transactions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 use std::error::Error;
-use std::{env, io, process};
+use std::{env, process};
 
 use csv::{ReaderBuilder, Trim};
 use serde::Deserialize;
@@ -37,6 +38,29 @@ impl TransactionType {
     }
 }
 
+#[derive(Debug)]
+struct Account {
+    client_id: u16,
+    available: f64,
+    held: f64,
+    total: f64,
+}
+
+impl Account {
+    fn new(client_id: &u16) -> Account {
+        Account {
+            client_id: client_id.to_owned(),
+            available: 0f64,
+            held: 0f64,
+            total: 0f64,
+        }
+    }
+
+    fn total(&mut self) {
+        self.total = self.available + self.held
+    }
+}
+
 // From a filepath, read a .csv file for transaction data
 // Deserialize data to Transaction structs
 fn get_csv_transactions_from_filepath(path: &str) -> Result<Vec<Transaction>, Box<dyn Error>> {
@@ -50,6 +74,14 @@ fn get_csv_transactions_from_filepath(path: &str) -> Result<Vec<Transaction>, Bo
     Ok(transactions)
 }
 
+fn apply_transaction(account: &mut Account, transaction: Transaction) {
+    match transaction.transaction_type {
+        TransactionType::Deposit => account.available += transaction.amount,
+        _ => {}
+    };
+    account.total();
+}
+
 fn main() {
     // TODO: add logging crate
     println!("*** Contabile *** \nStarting up!");
@@ -60,19 +92,37 @@ fn main() {
     println!("{:?}", transactions_filepath);
 
     // Read csv input file
-    let transactionss: Vec<Transaction> = match get_csv_transactions_from_filepath(transactions_filepath) {
-        Ok(transactions) => {
-            println!(
-                "Success reading csv input file csv file, {:?} transactions",
-                transactions.len()
-            );
-            transactions
-        }
-        Err(err) => {
-            println!("Error reading csv input file: {:?}", err);
-            process::exit(1);
-        }
-    };
+    let transactions: Vec<Transaction> =
+        match get_csv_transactions_from_filepath(transactions_filepath) {
+            Ok(transactions) => {
+                println!(
+                    "Success reading csv input file csv file, {:?} transactions",
+                    transactions.len()
+                );
+                transactions
+            }
+            Err(err) => {
+                println!("Error reading csv input file: {:?}", err);
+                process::exit(1);
+            }
+        };
 
+    let mut account_map: HashMap<u16, Account> = HashMap::new();
 
+    // Iterate over transactions and apply to accounts
+    for transaction in transactions {
+        if let Some(account) = account_map.get_mut(&transaction.client_id) {
+            apply_transaction(account, transaction);
+        } else {
+            let mut account = Account::new(&transaction.client_id);
+            let client_id = transaction.client_id;
+            apply_transaction(&mut account, transaction);
+            account_map.insert(client_id, account);
+        }
+    }
+
+    // For debugging purposes
+    for item in account_map {
+        println!("{:?}", item.1);
+    }
 }
